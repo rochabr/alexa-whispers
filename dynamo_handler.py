772d76df5f3d59a927562d2569edd4183a1318aa
Fernando Rocha <fernandorsl@gmail.com>
2019-02-24 23:16:32 -0600

    Update dynamo_handler.py

--- dynamo_handler.py
+++ dynamo_handler.py
@@ -1,7 +1,8 @@
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
 import json
-import decimal
+#import decimal
+from boto3.dynamodb.conditions import Key, Attr
 
 # Helper class to convert a DynamoDB item to JSON.
 # class DecimalEncoder(json.JSONEncoder):
@@ -18,29 +19,51 @@ def write_whisper(whisper):
 
     table = dynamodb.Table('whispers')
     
-    table.put_item(
+    ext_whisper = table.query(
+        KeyConditionExpression=Key('userId').eq(whisper.userId)
+    )
+    
+    if ext_whisper['Count'] == 0:
+        whisper_list = []
+        whisper_list.append({
+            'userId': whisper.userId,
+            'whisper': whisper.whisper,
+            'password': whisper.password,
+            'to':whisper.to
+        })
+        
+        table.put_item(
             Item={
-                'userId': "aaaaaa",
-                'username': "name",
-                'lastname': "last"
+                'userId': whisper.userId,
+                'whispers': whisper_list
             }
             )
-    
-    return "aaaa"
+    else:
+        whisper_list = ext_whisper['Items'][0]['whispers']
+        
+        whisper_list.append({
+            'userId': whisper.userId,
+            'whisper': whisper.whisper,
+            'password': whisper.password,
+            'to':whisper.to
+        })
+        table.update_item(
+            Key={
+                'userId': whisper.userId
+            },
+            UpdateExpression='SET whispers = :whisper_list',
+            ExpressionAttributeValues={
+                ':whisper_list': whisper_list
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    return True
 
-# title = "The Big New Movie"
-# year = 2015
+class Whisper:
+    def __init__(self, userId, whisper, password, to):
+        self.userId = userId
+        self.whisper = whisper
+        self.password = password
+        self.to = to
 
-# response = table.put_item(
-#   Item={
-#         'year': year,
-#         'title': title,
-#         'info': {
-#             'plot':"Nothing happens at all.",
-#             'rating': decimal.Decimal(0)
-#         }
-#     }
-# )
-
-# print("PutItem succeeded:")
 # print(json.dumps(response, indent=4, cls=DecimalEncoder))
